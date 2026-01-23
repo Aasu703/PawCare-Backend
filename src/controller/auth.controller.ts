96dbbe7c13163ff551079e15f952133f8644ce99
Aayush Subedi <smellylips508@gmail.com>
2026-01-23 11:31:39 +0545

    multer changes

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { UserService } from "../services/user.service";
-import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO, UpdateUserDto } from "../dtos/user.dto";
 import { Request, Response } from "express";
 import z, { date, success } from "zod";
 import { ca } from "zod/v4/locales";
@@ -74,6 +74,39 @@ export class AuthController {
                 { success: true, message: "User promoted to admin", data: updatedUser }
             );
         } catch (error: Error | any) {
+            return res.status(error.statusCode ?? 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            );
+        }
+    }
+
+    async updateUser(req: Request, res: Response) {
+        try {
+            const userId = req.user?._id;
+            if(!userId){
+                return res.status(401).json(
+                    { success: false, message: "Unauthorized" }
+                )
+            }
+            const parsedData = UpdateUserDto.safeParse(req.body);
+            if (!parsedData.success) {
+                return res.status(400).json(
+                    { success: false, message: z.prettifyError(parsedData.error) }
+                )
+            }
+
+            if(req.file){
+                parsedData.data.imageUrl = `/uploads/${req.file.filename}`; 
+            }
+
+
+            const updateData: UpdateUserDto = parsedData.data;
+            const updatedUser = await userService.updateUser(userId, updateData);
+            return res.status(200).json(
+                { success: true, message: "User updated successfully", data: updatedUser }
+            );
+        }
+        catch (error: Error | any) {
             return res.status(error.statusCode ?? 500).json(
                 { success: false, message: error.message || "Internal Server Error" }
             );
